13879567ad472777b6dc6561d37202b573e1e5a4
Md-Asifullah <artisan.asif@gmail.com>
2024-12-30 23:08:23 +0800

    version 1.2.4

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -11,7 +11,7 @@ export const useAuthStore = defineStore('auth', () => {
     let timer;
 
     // Access the API key from environment variables
-    const apiKey = import.meta.env.VITE_API_KEY;
+    const apiKey = process.env.VUE_APP_API_KEY;
 
     const signin = async (payload) => {
         try {
